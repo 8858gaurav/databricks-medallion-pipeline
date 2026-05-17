bb7c8c148670da9a91863ba5ff05539465da3fa6
8858gaurav <gauravmishra010@gmail.com>
2026-05-17 15:22:27 +0000

    test_30

--- silver/customers/silver_processing_customers.py
+++ silver/customers/silver_processing_customers.py
@@ -1,4 +1,13 @@
 from pyspark.sql.functions import col, upper, trim, current_timestamp
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("Hive-Warehouse-Delta") \
+    .enableHiveSupport() \
+    .config("spark.sql.warehouse.dir", "abfss://warehouse@misgauravstorageaccount.dfs.core.windows.net/") \
+    .config("spark.sql.catalog.misgauravcatalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .getOrCreate()
 
 # 1. Path Configurations
 input_base = "abfss://bronze@misgauravstorageaccount.dfs.core.windows.net/customers/"
