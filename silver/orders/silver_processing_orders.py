c38eba08e0427b12bf7f674910eb4bd2417583de
8858gaurav <gauravmishra010@gmail.com>
2026-05-17 23:26:53 +0000

    test-30

--- silver/orders/silver_processing_orders.py
+++ silver/orders/silver_processing_orders.py
@@ -1,9 +1,4 @@
 from pyspark.sql.functions import col, upper, trim, current_timestamp
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder \
-    .appName("cleaned_orders") \
-    .getOrCreate()
 
 # 1. Path Configurations
 input_base = "abfss://bronze@misgauravstorageaccount.dfs.core.windows.net/orders/"
@@ -11,9 +6,6 @@ output_base = "abfss://silver@misgauravstorageaccount.dfs.core.windows.net/order
 silver_checkpoint = "abfss://silver@misgauravstorageaccount.dfs.core.windows.net/_checkpoints/orders/"
 # 1. Path for data processing offsets
 offset_path = silver_checkpoint + "offsets"
-
-spark.sql("USE CATALOG misgauravcatalog")
-spark.sql("""CREATE SCHEMA IF NOT EXISTS retaildb""")
 
 # 1. Read from the Bronze folder in ADLS
 bronze_df = (spark.readStream
@@ -38,7 +30,7 @@ query = (silver_df.writeStream
     .outputMode('append') 
     .option("path", output_base) 
     .trigger(availableNow=True) 
-    .toTable('retaildb.silver_order_data')
+    .toTable('silver_order_data')
 )
 
 print("Streaming query started. Processing available batch data...")
@@ -54,6 +46,6 @@ print("Running file compaction and Z-Ordering maintenance...")
 # delta.autoOptimize.optimizeWrite = true ; before writing to the disk many small files are combine them to form a larger files (128MB), created bigger files (128MB). create a files around 128 MB after clubbing ; 
 # delta.autoOptimize.autoCompact = true ; small files are already written to the disk, then compacted to form larger files (128MB), works only when we have > 50 smaill files. create a files around 128 MB after clubbing ; 
 # 4. Maintenance: Now it is 100% safe to optimize because the data is fully written
-spark.sql("OPTIMIZE retaildb.silver_order_data ZORDER BY order_id")
+spark.sql("OPTIMIZE silver_order_data ZORDER BY order_id")
 
 print("Optimization and Z-Ordering Complete.")
